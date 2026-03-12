ac3681979db8a010557fdf3dedeed1cd2544e9ff
rishabhyadav@gmail.com <rishabhyadav14065@gmail.com>
2026-03-13 00:13:16 +0530

    commentary get fix

--- src/routes/commentary.js
+++ src/routes/commentary.js
@@ -1,17 +1,42 @@
 import {Router} from "express";
-import { createCommentarySchema } from "../validation/commentary.js";
+import { createCommentarySchema, listCommentaryQuerySchema } from "../validation/commentary.js";
 import { matchIdParamSchema } from "../validation/matches.js";
 import { commentary } from "../db/schema.js";
 import { db } from "../db/db.js";
+import { eq, desc } from "drizzle-orm";
 
 
 const MAX_LIMIT = 100;
 
 export const commentaryRouter = Router({ mergeParams: true });
 
-commentaryRouter.get('/',(req,res)=>{
+commentaryRouter.get('/',async (req,res)=>{
   const paramsResult = matchIdParamSchema.safeParse(req.params);
 
+  if(!paramsResult.success){
+     return res.status(400).json({error: 'Invalid Match Id', details: paramsResult.error.issues});
+  }
+
+  const queryResult = listCommentaryQuerySchema.safeParse(req.query);
+  if(!queryResult.success){
+     return res.status(400).json({error: 'Invalid Query Params', details: queryResult.error.issues});
+  }
+
+  try{
+    const { id: matchId } = paramsResult.data;
+    const { limit = 10 } = queryResult.data;
+
+    const safeLimit = Math.min(limit, MAX_LIMIT);
+
+    const results = await db.select().from(commentary).where(eq(commentary.matchId, matchId)).orderBy(desc(commentary.createdAt)).limit(safeLimit);
+
+    res.status(200).json({ data: results});
+  }
+  catch(error){
+        console.error('Failed to fetch commentary', error);
+    res.status(500).json({ error: 'Failed to fetch commentary'});
+
+  }
   
 });
 
