5b6233777a9dcada49a3219c2141d351afccd5db
rishabhyadav@gmail.com <rishabhyadav14065@gmail.com>
2026-03-14 12:34:28 +0530

    broadcasting to subscribers only

--- src/routes/commentary.js
+++ src/routes/commentary.js
@@ -55,12 +55,16 @@ commentaryRouter.post('/', async (req, res)=>{
   }
 
   try{
-    const {minutes, ...rest } = bodyResult.data;
+    const {minute, ...rest } = bodyResult.data;
     const [result] = await db.insert(commentary).values({
       matchId:paramsResult.data.id,
-      minutes,
+      minute,
       ...rest
     }).returning();
+    
+    if(res.app.locals.broadcastCommentary){
+      res.app.locals.broadcastCommentary(result.matchId, result); 
+    }
 
     res.status(201).json({data: result});
     
